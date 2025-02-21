ab4466628e51addccb85978f9fe3aa87c925c3f7
somsak <somsakk@tisco.co.th>
2025-02-21 15:27:52 +0700

    init project.

---
+++ ios_noti/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios_noti
+//
+//  Created by somsak02061 on 21/2/2568 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ios_notiTests/ios_notiTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_notiTests.swift
+//  ios_notiTests
+//
+//  Created by somsak02061 on 21/2/2568 BE.
+//
+
+import Testing
+@testable import ios_noti
+
+struct ios_notiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
